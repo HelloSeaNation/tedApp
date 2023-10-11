e41b7b897fd6e468e1fb17666dd024a56d151120
HelloSeaNation <sookann@gmail.com>
2023-10-11 20:47:04 +1300

    correction in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import Express from "express";
+import path from "path";
 
 const app = Express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(Express.static(path.join(__dirname, 'frontend')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send(path.join(__dirname, 'frontend', 'index.html'));
 });
