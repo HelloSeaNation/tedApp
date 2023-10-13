ef9251e52a2436e9c61dfeda6e129073cc6a98be
HelloSeaNation <sookann@gmail.com>
2023-10-13 21:00:40 +1300

    revert call

--- src/app.ts
+++ src/app.ts
@@ -15,18 +15,18 @@ app.get("/", (req, res) => {
     res.send(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-app.get("/order", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'order.html'));
-});
+// app.get("/order", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'order.html'));
+// });
 
-app.get("/cart", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'cart.html'));
-});
+// app.get("/cart", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'cart.html'));
+// });
 
-app.get("/cart/payment", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'payment.html'));
-});
+// app.get("/cart/payment", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'payment.html'));
+// });
 
-app.get("/thankyou", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'thankyou.html'));
-});
+// app.get("/thankyou", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'thankyou.html'));
+// });
